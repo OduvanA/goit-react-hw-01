c35bb7d51e3b29c0ba2d2fef2bda52feb227233b
OduvanA <145233058+OduvanA@users.noreply.github.com>
2024-06-16 17:35:27 -0500

    first component

---
+++ src/components/App/App.jsx
@@ -0,0 +1,20 @@
+import css from './App.module.css'
+import { Profile } from '../Profile/Profile';
+import userData from "../userData.json";
+import friends from "../friends.json";
+import transactions from "../transactions.json";
+
+
+export default function App() {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
